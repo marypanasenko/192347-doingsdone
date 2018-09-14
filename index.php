a90066a4107994764e47ae08ead791bc99570383
Maria Panasenko <kerch.pm@gmail.com>
2018-09-14 17:21:05 +0300

    Исправила ошибку

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $show_complete_tasks = rand(0, 1);
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
                         <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                            <?php if ($show_complete_tasks == 1): ?>
+                            <?php if ($show_complete_tasks): ?>
                                 checked
                             <?php endif; ?>
                         >
@@ -100,7 +100,7 @@ $show_complete_tasks = rand(0, 1);
 
                         <td class="task__date"></td>
                     </tr>
-                    <?php if ($show_complete_tasks == 1): ?>
+                    <?php if ($show_complete_tasks): ?>
                         <tr class="tasks__item task task--completed">
                             <td class="task__select">
                                 <label class="checkbox task__checkbox">
