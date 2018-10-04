e1de49cb585a7077f3083332f931b0a865bc9dfb
Maria Panasenko <kerch.pm@gmail.com>
2018-10-04 10:55:07 +0300

    #13 Работа со строкой запроса

--- index.php
+++ index.php
@@ -15,13 +15,16 @@ if (!$connection) {
 
 $projects = projects_sql($current_user, $connection);
 $tasks = tasks_sql($current_user, $connection, $add_and);
-$array_result = project_id($current_user, $connection, $project_id);
 
-if ($array_result !== null and !$array_result) {
-    header("HTTP/1.0 404 Not Found");
-    die();
+
+if (isset($_GET["project_id"])) {
+    $array_result = project_id($current_user, $connection, $project_id);
+
+    if ($array_result == null || !$array_result) {
+        header("HTTP/1.0 404 Not Found");
+        die();
+    }
 }
-
 
 $page_content = include_template("index.php", [
     "tasks" => $tasks,
